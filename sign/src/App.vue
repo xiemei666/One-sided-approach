dbe2af30001d2f9cabaa134eda85ef94ce44ce14
xiemei <799770735@qq.com>
2019-08-06 19:16:47 +0800

    添加面试

--- sign/src/App.vue
+++ sign/src/App.vue
@@ -30,20 +30,17 @@ export default {
 </script>
 
 <style>
-.container {
+page{
+  width: 100%;
   height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: 200rpx 0;
-  box-sizing: border-box;
 }
 /* this rule will be remove */
 * {
-  transition: width 2s;
-  -moz-transition: width 2s;
-  -webkit-transition: width 2s;
-  -o-transition: width 2s;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+html,body,div,p,span,ul,ol,li{
+  box-sizing: border-box;
 }
 </style>
---
+++ sign/src/pages/personalCenter/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="mainWarp">
+    213
+  </div>
+</template>
+
+<script>
+// Use Vuex
+// import store from "./store";
+
+export default {
+  computed: {
+    // count() {
+    //   return store.state.count;
+    // }
+  },
+  methods: {
+    // increment() {
+    //   store.commit("increment");
+    // },
+    // decrement() {
+    //   store.commit("decrement");
+    // }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
